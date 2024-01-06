c90df7c29e709f2f74fb8a51b946827aa05e8498
eodaneze <ezealiokechukwu1999@gmail.com>
2024-01-06 14:13:31 -0800

    setting up the routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Menu from './pages/Menu'
+import Service from './pages/Service'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+       <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<Homepage />}/>
+            <Route path='/menu' element={<Menu />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/service' element={<Service />}/>
+            <Route path='/contact' element={<Contact />}/>
+         </Routes>
+       </BrowserRouter>
+    </div>
   )
 }
 
